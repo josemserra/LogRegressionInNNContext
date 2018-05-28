c60858728e3c201b17e81ccc5898fd1ba62cbcce
JoseLaptop <joseserra@outlook.com>
2018-05-28 12:05:03 +0100

    - Added Eigen

--- LogRegressionInNNContext/main.cpp
+++ LogRegressionInNNContext/main.cpp
@@ -1,9 +1,24 @@
 #include "CImg.h"
+
+#include <iostream>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
 using namespace cimg_library;
+
 int main() {
+
+	//Eigen Hello World
+	MatrixXd m(2, 2);
+	m(0, 0) = 3;
+	m(1, 0) = 2.5;
+	m(0, 1) = -1;
+	m(1, 1) = m(1, 0) + m(0, 1);
+	std::cout << m << std::endl;
+
+	//CImg Hello World
 	CImg<unsigned char> image("../Img/lena.jpg"), visu(500, 400, 1, 3, 0);
 	const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
-	image.blur(2.5);
 	CImgDisplay main_disp(image, "Click a point"), draw_disp(visu, "Intensity profile");
 	while (!main_disp.is_closed() && !draw_disp.is_closed()) {
 		main_disp.wait();
@@ -14,5 +29,6 @@ int main() {
 			visu.draw_graph(image.get_crop(0, y, 0, 2, image.width() - 1, y, 0, 2), blue, 1, 1, 0, 255, 0).display(draw_disp);
 		}
 	}
+
 	return 0;
 }
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Binary Classification with Logistic Regression
 
 Visual Studio (2015) project with my code for binay classification using logistic regression. This was the first step to delve deeper into deep learning
 
-- CImg uses external libs to process the images. I installed this one and it solved the problems that I had ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/windows/ (more specifically version GraphicsMagick-1.3.27-Q8-win64-dll, but I assume others will work)
+- CImg (http://cimg.eu/) uses external libs to process the images. I installed this one and it solved the problems that I had ftp://ftp.graphicsmagick.org/pub/GraphicsMagick/windows/ (more specifically version GraphicsMagick-1.3.27-Q8-win64-dll, but I assume others will work)
